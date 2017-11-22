da8b947d497ec1d45fe6543e7bebf2894eaea371
bmlikota <bmlikota@bmlikota-Inspiron-3543>
2017-11-22 22:53:42 +0100

    gitignore

--- workspace/ESP8266/.ino.cpp
+++ workspace/ESP8266/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-11-22 19:41:29
+//This file has been generated on 2017-11-22 22:48:24
 
 #include "Arduino.h"
 #include "ESP8266.h"
